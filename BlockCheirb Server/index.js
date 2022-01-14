131ff9acaa37fcbd900c50b8dccbf3e6d92e231e
Bahloul yahya <yahyabahloul1999@gmail.com>
2022-01-14 17:29:18 +0100

    heroku setup

--- BlockCheirb Server/index.js
+++ BlockCheirb Server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const Gun = require("gun");
+const path = require("path");
 
 const port = 3001;
 
 app.use(Gun.serve);
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
